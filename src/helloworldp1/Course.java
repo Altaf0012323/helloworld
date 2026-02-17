9eda738edd7323ddef933991e1b7507c89c59bf4
Altaf Dar <eraltaf001@gmail.com>
2026-02-17 23:07:42 +0530

    Refactor main method in Course.java

--- src/helloworldp1/Course.java
+++ src/helloworldp1/Course.java
@@ -2,8 +2,6 @@ package helloworldp1;
 
 public class Course {
 
-	public static void main(String[] args) {
-		System.out.println(" Automation Testing with Java selenium");
-	}
-
-}
+public static void main(String[] args) {
+System.out.println(" Automation Testing with Java selenium");
+}}
